e6ba72f7944340f236ce31214a6fe4cb7187935c
marcio <marcioqui3@gmail.com>
2021-11-03 11:21:38 +0000

    lj

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import Footer from "./componentes/Footer";
 import Nav from "./componentes/Nav";
 import Aside from "./componentes/Aside";
 import Row from "./paginas/Row";
-
+import Home from "./paginas/home/Home";
+import axios from "axios";
+axios.defaults.baseURL = "http://localhost:8800/api";
 function App() {
   return (
     <div className="App">
       <div className="wrapper">
         <Nav />
         <Aside />
-        <Row />
+        <Home />
         <Footer />
       </div>
     </div>
