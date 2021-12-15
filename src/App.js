8dcaed94763646438cf5663a8e89b56e8f667693
marcio <marcioqui3@gmail.com>
2021-12-15 08:01:54 +0000

    lo

--- src/App.js
+++ src/App.js
@@ -33,8 +33,8 @@ import IndicadorA from "./paginas/IndicadorA";
 import Usuario from "./paginas/Usuario";
 import Login from "./paginas/Login";
 
-axios.defaults.baseURL =
-  "https://emarpe-api.herokuapp.com/api" || "http://localhost:8800/api";
+axios.defaults.baseURL = "https://emarpe-api.herokuapp.com/api";
+//http://localhost:8800/api"
 
 function App() {
   return (
@@ -51,7 +51,7 @@ function App() {
             <Route path="/parametrizacao" element={<Parametrizacao />} />
             <Route path="/Variaveis" element={<Variaveis />} />
             <Route path="/ajudas" element={<Ajudas />} />
-            <Route path="/empresas" element={<Empresas />} />
+            <Route exact path="/empresas" element={<Empresas />} />
             <Route path="/planos" element={<Planos />} />
             <Route path="/indicador_Mensal" element={<IndicadorM />} />
             <Route path="/indicador_Trimestral" element={<IndicadorT />} />
